06467d4f6238ebf8642723a0b64710981ea25f00
gyoumanist <93202305+gyoumanist@users.noreply.github.com>
2022-05-13 21:10:00 +0900

    Router実装

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
-import { Page1 } from "../Page1";
-import { Page2 } from "../Page2";
+import { Page404 } from "../Page404";
 import { page1Routes } from "./Page1Routes";
+import { page2Routes } from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -13,11 +13,40 @@ export const Router = () => {
       <Route
         path="/page1"
         render={({ match: { url } }) => (
-          <Switch>{page1Routes.map(() => {})}</Switch>
+          <Switch>
+            {page1Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
         )}
       />
-      <Route path="/page2">
-        <Page2 />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {page2Routes.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
+      <Route path="*">
+        <Page404 />
       </Route>
     </Switch>
   );
